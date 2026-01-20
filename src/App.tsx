89f15bfd2e5ffc1dfb6fe5506c25d4305e6ba246
César Silva <casrr@proton.me>
2026-01-20 10:09:45 +0100

    add wait message while calculating sizes

--- src/App.tsx
+++ src/App.tsx
@@ -10,30 +10,47 @@ interface DirEntry {
 
 function App() {
   const [entries, setEntries] = useState<DirEntry[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function scan() {
-    const result = await invoke("scan_directory", {});
+    setIsLoading(true);
 
 
-    const lines = (result as string).trim().split("\n")
-    const parsed = lines
-    .filter(line => line.trim())
-    .map(line => {
-      const parts = line.trim().split((/\s+/));
-      const size = parts[0];
-      const path = parts.slice(1).join(" ")
-      const name = path.split("/").pop() || path;
-      return { size, name, path };
-    });
-    setEntries(parsed);
-  }
+    await new Promise(resolve => setTimeout(resolve, 50));
+
+    try{
+      const result = await invoke("scan_directory", {});
+      const lines = (result as string).trim().split("\n")
+      const parsed = lines
+      .filter(line => line.trim())
+      .map(line => {
+        const parts = line.trim().split((/\s+/));
+        const size = parts[0];
+        const path = parts.slice(1).join(" ")
+        const name = path.split("/").pop() || path;
+        return { size, name, path };
+      });
+
+      setEntries(parsed);
+    } catch (error) {
+      console.log("Scan failed:", error);
+    } finally {
+      setIsLoading(false);
+    }
+}
+   
+   
+    
 
   
 
   return (
     <main className="container">
     <h1>duSurfer</h1>
-    <button className="scan-button">Scan Home Directory</button>
+    <button onClick={scan} disabled={isLoading} className="scan-button">
+      {isLoading ? "Scanning..." : "Scan Home Directory"}
+    </button>
+    {isLoading && <p>Calculating folder sizes...</p>}
     
     <ul className="dir-list">
       {entries.map((entry, i) => (
